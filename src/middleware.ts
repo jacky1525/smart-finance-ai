0d6fed1d3051c41b7dae9a6aa75e17d1e36bab35
Hasan Fidan <hasanfidan2535@gmail.com>
2026-02-14 19:45:10 +0300

    smart-finance-ai

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 // middleware.ts
-export { auth as middleware } from "@/auth"
+import NextAuth from "next-auth";
+import { authConfig } from "./auth_config";
 
-// Sadece belirli sayfalarda çalışması için config:
+export default NextAuth(authConfig).auth;
+
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
+};
